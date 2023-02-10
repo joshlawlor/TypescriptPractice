30a3dbdcff794efc65c4509afd513e5bd0839ec3
Josh Lawlor <joshlawlor1@gmail.com>
2023-02-10 14:51:26 -0500

    Ability to view cart contents on cart page.

--- shop-app/app/cart/page.tsx
+++ shop-app/app/cart/page.tsx
@@ -1,11 +1,40 @@
 
+import Link from "next/link";
+
+async function getCart() {
+    const res = await fetch(`http://127.0.0.1:8090/api/collections/cart/records?page=1&perPage=30`, { cache: 'no-store' })
+    const data = await res.json();
+    return data?.items as any[];
+
+}
 
 
 export default async function CartPage({params}: any){
+    const products = await getCart()
 
     return(
         <div>
             <h1>Your Cart:</h1>
+
+            <div>
+            {products?.map((product) => {
+                    return <Product key={product.id} product={product} />;
+                })}
+            </div>
         </div>
+    )
+}
+
+function Product({ product }: any) {
+    const { id, title, price } = product || {};
+
+    return (
+        <Link href={`/products/${id}`}>
+            <div>
+                <h2>{title}</h2>
+                <h4>{price}</h4>
+            </div>
+
+        </Link>
     )
 }
